c0a9de245a4711bbde7ca641f290d7e6a31c2758
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-21 22:53:42 -0800

    Create TestOfKnowledge.java

---
+++ chapter-1/TestOfKnowledge.java
@@ -0,0 +1,19 @@
+/*
+* Write series of println statements that produce the following output
+* This is a test of your
+* knowledge of "quotes" used
+* in 'string literals.'
+*
+* You're bound to "get it right"
+* if you read the section on
+* ''quotes.''
+*/
+
+System.out.println("This is a test of your");
+System.out.println("knowledge of \"quotes\" used");
+System.out.println("in 'string literals.'");
+System.out.println();
+System.out.println("You're bound to \"get it right\"");
+System.out.println("if you read the section on");
+System.out.print("''quotes.''");
+
