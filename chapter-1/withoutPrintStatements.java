4222ee81042829a690206d8d1da1a19c77305328
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-21 22:56:01 -0800

    Create withoutPrintStatements.java

---
+++ chapter-1/withoutPrintStatements.java
@@ -0,0 +1,21 @@
+/*
+* Rewrite the following code as a series of equivalent System.out.println statements 
+* (i.e., without any System.out.print statements):
+*
+* System.out.print("Twas ");
+* System.out.print("brillig and the ");
+* System.out.println(" ");
+* System.out.print("  slithy toves did");
+* System.out.print(" ");
+* System.out.println("gyre and");
+* System.out.println(  "gimble");
+* System.out.println();
+* System.out.println(  "in the wabe." );
+*/
+
+System.out.println("Twas brillig and the  ");
+System.out.println("  slithy toves did gyre and");
+System.out.println(  "gimble");
+System.out.println();
+System.out.println(  "in the wabe." );
+
