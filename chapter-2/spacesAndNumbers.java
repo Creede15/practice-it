f18b56437b0d41bb60c35bad7c441fb10a51dbdd
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-22 21:53:50 -0800

    Create spacesAndNumbers.java

---
+++ chapter-2/spacesAndNumbers.java
@@ -0,0 +1,18 @@
+/*
+* Write nested for loops to produce the following output: 
+*     1
+*    22
+*   333
+*  4444
+* 55555
+*/
+
+for (int i = 1; i <= 5; i++) {
+    for (int j = 1; j <= 5 - i; j++) {
+        System.out.print(" ");
+    }
+    for (int k = 1; k <= i; k++) {
+        System.out.print(i);
+    }
+    System.out.println();
+}
