97a59c1d56f936a6273fe624a17deb9d8342e6d6
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-22 22:39:19 -0800

    Update DollarFigure.java

--- chapter-2/DollarFigure.java
+++ chapter-2/DollarFigure.java
@@ -12,30 +12,30 @@
 */
 
 public class DollarFigure {
-	public static void main(String[] args) {
-		for (int i = 0; i < 7; i++) {
+   public static void main(String[] args) {
+      for (int i = 0; i < 7; i++) {
 		
-			for (int j = 1; j <= 2 * i; j++) {
-				System.out.print("*");
-			}
+      for (int j = 1; j <= 2 * i; j++) {
+         System.out.print("*");
+      }
 			
-			for (int j = 1; j <= 7 - i; j++) {
-				System.out.print("$");
-			}
+      for (int j = 1; j <= 7 - i; j++) {
+	 System.out.print("$");
+      }
 			
-			for (int j = 1; j <= 14 - 2 * i; j++) {
-				System.out.print("*");
-			}
+      for (int j = 1; j <= 14 - 2 * i; j++) {
+         System.out.print("*");
+      }
+
+      for (int j = 1; j <= 7 - i; j++) {
+         System.out.print("$");
+      }
+	
+      for (int j = 1; j <= 2 * i; j++) {
+         System.out.print("*");
+      }
 			
-			for (int j = 1; j <= 7 - i; j++) {
-				System.out.print("$");
-			}
-			
-			for (int j = 1; j <= 2 * i; j++) {
-				System.out.print("*");
-			}
-			
-			System.out.println();
-		}
-	}
+      System.out.println();
+      }
+   }
 }
