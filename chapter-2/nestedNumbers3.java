cc1b6e16e8c312e8f48ac4786e89c9718ad3dcbd
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-22 22:05:54 -0800

    Create nestedNumbers3.java

---
+++ chapter-2/nestedNumbers3.java
@@ -0,0 +1,18 @@
+/*
+* Modify your code from the previous problem to produce the following output:
+* 
+* 999999999888888887777777666666555554444333221
+* 999999999888888887777777666666555554444333221
+* 999999999888888887777777666666555554444333221
+* 999999999888888887777777666666555554444333221
+*/
+
+for (int i = 1; i <= 4; i++) {
+    for (int j = 9; j >= 1; j--) {
+        for (int k = 1; k <= j; k++) {
+            System.out.print(j);
+        }
+    }
+    System.out.println();
+}
+
