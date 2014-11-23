d81a3d754d1d9e4bd3481ae58e8648c90c36b8cf
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-22 21:48:38 -0800

    Create starSquar.java

---
+++ chapter-2/starSquar.java
@@ -0,0 +1,13 @@
+/* Write for loops to produce the following output
+*  *****
+*  *****
+*  *****
+*  *****
+*/
+
+for (int i = 1; i <= 4; i++) {
+    for (int j = 1; j <= 5; j++) {
+        System.out.print("*");
+    }
+    System.out.println();
+}
