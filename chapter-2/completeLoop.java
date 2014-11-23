4b89dce9bb523c9c8ae862b8e6708b401366e9f6
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-22 21:38:57 -0800

    Create completeLoop.java

---
+++ chapter-2/completeLoop.java
@@ -0,0 +1,15 @@
+/*
+* Complete the following for loop to produce the following output:
+* 
+* -4
+* 14
+* 32
+* 50
+* 68
+* 86
+*/
+
+for (int i = 1; i <= 6; i++) {
+    System.out.println(-22 + 18 * i);
+}
+
