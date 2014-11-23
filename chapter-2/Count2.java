79600c46931281c75735a46421bc61fb3609627f
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-22 21:35:18 -0800

    Create Count2.java

---
+++ chapter-2/Count2.java
@@ -0,0 +1,15 @@
+/*
+* Write your own code to produce the following output:
+* 2 times 1 = 2
+* 2 times 2 = 4
+* 2 times 3 = 6
+* 2 times 4 = 8
+*/
+
+public class Count2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            System.out.println("2 times " + i + " = " + 2*i);
+        }
+    }
+}
