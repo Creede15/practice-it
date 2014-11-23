6ef4ecd8433d15618f6b5f6dfc884c919e230dee
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-22 21:56:24 -0800

    Update spacesAndNumbers.java

--- chapter-2/spacesAndNumbers.java
+++ chapter-2/spacesAndNumbers.java
@@ -10,9 +10,11 @@
 for (int i = 1; i <= 5; i++) {
     for (int j = 1; j <= 5 - i; j++) {
         System.out.print(" ");
-    }
+    } 
+    
     for (int k = 1; k <= i; k++) {
         System.out.print(i);
     }
+    
     System.out.println();
 }
