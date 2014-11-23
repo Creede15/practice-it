4258d46fab5f71b8f158b04819441242e6a05183
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-22 21:57:00 -0800

    Update waveNumbers40.java

--- chapter-2/waveNumbers40.java
+++ chapter-2/waveNumbers40.java
@@ -9,15 +9,21 @@
 for (int a = 1; a <= 40; a++) {
     System.out.print("-");
 }
+
 System.out.println();
+
 for (int b = 1; b <= 10; b++) {
     System.out.print("_-^-");
 }
+
 System.out.println();
+
 for (int c = 1; c <= 2; c++) {
     System.out.print("11223344556677889900");
 }
+
 System.out.println();
+
 for (int a = 1; a <= 40; a++) {
     System.out.print("-");
 }
