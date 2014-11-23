ad9e9991856f9e7558892d6bb7b65fbfa1d59df6
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-22 21:37:07 -0800

    Update Count2.java

--- chapter-2/Count2.java
+++ chapter-2/Count2.java
@@ -9,7 +9,7 @@
 public class Count2 {
     public static void main(String[] args) {
         for (int i = 1; i <= 4; i++) {
-            System.out.println("2 times " + i + " = " + 2*i);
+            System.out.println("2 times " + i + " = " + 2 * i);
         }
     }
 }
