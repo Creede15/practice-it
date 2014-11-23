7282369919784202930650762bcbefa36342690b
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-22 21:33:37 -0800

    Create timesOperator.java

---
+++ chapter-2/timesOperator.java
@@ -0,0 +1,9 @@
+/*
+* Suppose you have a real number variable x. Write a single Java statement that 
+* declares and computes a variable y storing the result of the following computation,
+* but modify the expression to use the * operator only four times while computing an equivalent value.
+*
+* y = 12.3x4 - 9.1x3 + 19.3x2 - 4.6x + 34.2
+*/
+
+double y = x*(x*(19.3 + x*(12.3*x - 9.1))- 4.6) + 34.2;
