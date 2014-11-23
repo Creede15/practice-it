0b5fc197ce74eab127351fd0ff981460ad581575
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-22 22:21:10 -0800

    Create Window.java

---
+++ chapter-2/Window.java
@@ -0,0 +1,60 @@
+/* 
+* This program produces the following output:
+* 
+* +===+===+
+* |   |   |
+* |   |   |
+* |   |   |
+* +===+===+
+* |   |   |
+* |   |   |
+* |   |   |
+* +===+===+
+*
+* size of the figure can be changed simply by changing that constant's value.
+*/
+
+public class Window {
+  public static final int size = 6;
+	public static void main(String[] args) {
+		line();
+		verticalLine();
+		line();
+		verticalLine();
+		line();
+	}
+	
+	public static void line() {
+		System.out.print("+");
+		
+		for (int i = 1; i <= size; i++) {
+			System.out.print("=");
+		}
+		
+		System.out.print("+");
+		
+		for (int i = 1; i <= size; i++) {
+			System.out.print("=");
+		}
+		
+		System.out.println("+");
+	}
+	
+	public static void verticalLine() {
+		for (int i = 1; i <= size; i++) {
+			System.out.print("|");
+			
+			for (int j = 1; j <= size; j++) {
+				System.out.print(" ");
+			}
+			
+			System.out.print("|");
+			
+			for (int j = 1; j <= size; j++) {
+				System.out.print(" ");
+			}
+			
+			System.out.println("|");
+		}
+	} 
+}
