a62d30407b923694215072f3c63309d76334b17d
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-22 21:49:54 -0800

    Create starTriangle.java

---
+++ chapter-2/starTriangle.java
@@ -0,0 +1,15 @@
+/*
+* Write for loops to produce the following output:
+*  *
+*  **
+*  ***
+*  ****
+*  *****
+*/
+
+for (int i = 1; i <= 5; i++) {
+    for (int j = 1; j <= i; j++) {
+        System.out.print("*");
+    }
+    System.out.println();
+}
