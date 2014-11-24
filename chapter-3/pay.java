59a9db923f41fcfdca3e241d3e1ec815eae73d6b
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-23 21:13:27 -0800

    Update pay.java

--- chapter-3/pay.java
+++ chapter-3/pay.java
@@ -9,7 +9,7 @@
 
 public static double pay(double hourSalary, int hour) {
     double salary = 0;
-    if(hour > 8) {
+    if (hour > 8) {
         salary = 8 * hourSalary + (hour - 8) * (1.5 * hourSalary);
     } else {
         salary = hour * hourSalary;
