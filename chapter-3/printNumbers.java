45b182e3b0650a96bd02936a6187c60d48080138
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-23 20:42:22 -0800

    Create printNumbers.java

---
+++ chapter-3/printNumbers.java
@@ -0,0 +1,14 @@
+/*
+* Write a method called printNumbers that accepts a maximum number as a parameter and prints each 
+* number from 1 up to that maximum, inclusive, boxed by square brackets.
+*/
+
+public static void printNumbers(int max) {
+    for(int i = 1; i <= max; i++) {
+        System.out.print("[");
+        System.out.print(i);
+        System.out.print("] ");
+    }
+    
+    System.out.println();
+}
