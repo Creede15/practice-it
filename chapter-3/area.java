6592621bbadf3a11e6388b9624975c2ba57c582c
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-23 20:55:05 -0800

    Create area.java

---
+++ chapter-3/area.java
@@ -0,0 +1,8 @@
+/*
+* Write a method named area that accepts the radius of a circle as a parameter 
+* and returns the area of a circle with that radius.
+*/
+
+public static double area(double radius) {
+    return Math.PI * radius * radius;
+}
