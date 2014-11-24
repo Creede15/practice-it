1eac2e05b9d93f424743849b24b7858689acc278
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-23 21:00:28 -0800

    Create sphereVolume.java

---
+++ chapter-3/sphereVolume.java
@@ -0,0 +1,8 @@
+/*
+* Write a method called sphereVolume that accepts a radius (a real number) 
+* as a parameter and returns the volume of a sphere with that radius.
+*/
+
+public static double sphereVolume(double radius) {
+    return 4 * Math.PI * Math.pow(radius, 3) / 3;
+}
