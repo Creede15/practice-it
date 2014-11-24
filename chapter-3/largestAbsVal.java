28dfdb246a9bdd40865a4aaff9c04b4f1c7287f3
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-23 20:51:12 -0800

    Create largestAbsVal.java

---
+++ chapter-3/largestAbsVal.java
@@ -0,0 +1,8 @@
+/* 
+* Write a method largestAbsVal that accepts three integers as parameters 
+* and returns the largest of their three absolute values. 
+*/
+
+public static int largestAbsVal(int a, int b, int c) {
+    return Math.max(Math.abs(a), Math.max(Math.abs(b), Math.abs(c)));
+}
