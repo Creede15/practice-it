3e14c323e967bcd2e6f37fe9c12bb78d1c9963e9
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-23 20:39:52 -0800

    Create min.java

---
+++ chapter-3/min.java
@@ -0,0 +1,8 @@
+/*
+* Write a method called min that takes three integers as parameters 
+* and returns the smallest of the three values
+*/
+
+public static int min(int a, int b, int c){
+    return Math.min(a, Math.min(b, c));
+}
