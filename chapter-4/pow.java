93b58a29ac5ec6570968fa2f641c98cd870b4bf3
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-24 20:39:50 -0800

    Create pow.java

---
+++ chapter-4/pow.java
@@ -0,0 +1,16 @@
+/*
+* Write a method named pow that accepts a base and an exponent as parameters 
+* and returns the base raised to the given power. For example, the call pow(3, 4) 
+* returns 3 * 3 * 3 * 3 or 81. Do not use Math.pow in your solution. Assume 
+* that the base and exponent are non-negative. 
+*/
+
+public static int pow(int base, int exp) {
+    int product = 1;
+    
+    for (int i = 0; i < exp; i++) {
+        product *= base;
+    }
+    
+    return product;
+}
