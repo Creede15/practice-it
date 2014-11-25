4d9611f558c9afa0b18020f550052f7713434f5f
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-24 20:55:24 -0800

    Create average.java

---
+++ chapter-4/average.java
@@ -0,0 +1,8 @@
+/*
+* Write a method called average that takes two integers as parameters
+* and returns the average of the two integers. 
+*/
+
+public static double average(int a, int b) {
+    return (a + b) / 2.0;
+}
