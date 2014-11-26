8f5983aa29c4cc11c2f48f39b3ecf7c6a28cafa0
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-26 15:56:12 -0800

    Update wordWrap.java

--- chapter-6/wordWrap.java
+++ chapter-6/wordWrap.java
@@ -4,18 +4,20 @@
 * console, word-wrapping all lines that are longer than 60 characters. 
 */
 
-public static void wordWrap(Scanner input){
-    while(input.hasNextLine()){
+public static void wordWrap(Scanner input) {
+    while (input.hasNextLine()) {
         String line = input.nextLine();
         int length = line.length();
         int startPoint = 0;
         int endPoint = 60;
-        while(length > 60){
+        
+        while (length > 60) {
             System.out.println(line.substring(startPoint, endPoint));
             startPoint += 60;
             endPoint += 60;
             length -= 60;
         }
+        
         System.out.println(line.substring(startPoint, line.length()));
     }
 }
