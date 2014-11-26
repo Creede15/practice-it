2d9f97134e98d4fdc12d8f843c7166e27e5fa17f
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-26 15:42:02 -0800

    Create boyGirl.java

---
+++ chapter-6/boyGirl.java
@@ -0,0 +1,34 @@
+/*
+* Write a method named boyGirl that accepts a Scanner as a parameter. The 
+* Scanner is reading its input from a file containing a series of names 
+* followed by integers. The names alternate between boys' names and girls' 
+* names. Your method should compute the absolute difference between the 
+* sum of the boys' integers and the sum of the girls' integers. The input
+* could end with either a boy or girl; you may not assume that it contains 
+* an even number of names. If the input file tas.txt contains the following text:
+*
+* Steve 3 Sylvia 7 Craig 14 Lisa 13 Brian 4 Charlotte 9 Jordan 6
+*/
+
+public static void boyGirl(Scanner console) {
+    int boy = 0;
+    int bSum = 0;
+    int girl = 0;
+    int gSum = 0;
+    int i = 2;
+    
+    while (console.hasNext()) {
+        console.next();
+        if ((i / 2) % 2 != 0) {
+            bSum += console.nextInt();
+            boy++;
+        } else {
+            gSum += console.nextInt();
+            girl++;
+        }
+        i += 2;
+    }
+    
+    System.out.println(boy + " boys, " + girl + " girls");
+    System.out.println("Difference between boys' and girls' sums: " + Math.abs(bSum - gSum));
+}
