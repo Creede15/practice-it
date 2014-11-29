6c3bff507feb40d78f6d42b68401a69773b22723
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-29 11:25:55 -0800

    Create Marketer.java

---
+++ chapter-9/Marketer.java
@@ -0,0 +1,20 @@
+/*
+* Write the class Marketer to accompany the other law firm classes described in this chapter. 
+* Marketers make $50,000 ($10,000 more than general employees) and have an additional method 
+* called advertise that prints "Act now, while supplies last!" Make sure to interact with 
+* the Employee superclass as appropriate. 
+*/
+
+public class Marketer extends Employee {
+    public Marketer () {
+        super();
+    }
+    
+    public double getSalary() {
+        return super.getSalary() + 10000;
+    }
+    
+    public void advertise() {
+        System.out.println("Act now, while supplies last!");
+    }
+}
