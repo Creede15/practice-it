9ae27458e860f55778a61b7b8278d14b60c06a33
jasonbai2014 <jasonjason20062006@yahoo.com>
2014-11-28 18:42:43 -0800

    Create getSlopeLine.java

---
+++ chapter-8/getSlopeLine.java
@@ -0,0 +1,24 @@
+/*
+* Add the following method to your Line class from the previous exercise:
+* 
+* public double getSlope()
+* 
+* Returns the slope of this line. The slope of a line between points (x1, y1) and (x2, 
+* y2) is equal to (y2 - y1) / (x2 - x1). If the two points have the same x-coordinates, 
+* the denominator is zero and the slope is undefined, so you should throw an 
+* IllegalStateException in this case. 
+*/
+
+public double getSlope() {
+    double x1 = p1.getX();
+    double x2 = p2.getX();
+    double y1 = p1.getY();
+    double y2 = p2.getY();
+    if (x1 == x2) {
+        throw new IllegalStateException();
+    } else if (y1 == y2) {
+        return 0;
+    } else {
+        return (y2 - y1) / (x2 - x1);
+    }
+}
